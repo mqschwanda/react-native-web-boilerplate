70dc449e0d15d35f79c98e7a64a53ef6f84a95f1
mqschwanda <mqschwanda@gmail.com>
2018-09-10 15:02:12 -0400

    refactor for common app

--- packages/web/config-overrides.js
+++ packages/web/config-overrides.js
@@ -1,8 +1,8 @@
-module.exports = require('config-workspaces').getWebpackConfig(__dirname);
+module.exports = require('@config/workspaces').getWebpackConfig(__dirname);
 //
 // or...
 //
-// const webpackConfig = require('config-workspaces').getWebpackConfig(__dirname);
+// const webpackConfig = require('@config/workspaces').getWebpackConfig(__dirname);
 //
 // module.exports = function override(oldConfig, env) {
 //   const newConfig = webpackConfig(oldConfig, env);
